0ca37812fababc4a634ff71204efb9dddad3bb6c
Scotow <contact@scotow.com>
2018-03-11 00:12:31 +0100

    Add DH signature verification

--- dh/session.js
+++ dh/session.js
@@ -1,27 +1,35 @@
 // Modules.
+const path = require('path');
 const crypto = require('crypto');
 const fs = require('fs-extra');
-// const tmp = require('tmp-promise');
+const tmp = require('tmp-promise');
 const { exec } = require('child-process-promise');
 const request = require('request-promise-native').defaults({
-    baseUrl: 'http://pac.fil.cool/uglix/bin/',
-    jar: true
+    baseUrl: 'http://pac.fil.cool/uglix/bin/'
 });
 const bigInt = require("big-integer");
 
 // Environment variables.
 require('dotenv').config();
 
+// Cookie jar.
+let jar;
+
 if(!process.env.PAC_USERNAME) throw new Error('Please set PAC_USERNAME environment variable.');
 if(!process.argv[2]) throw new Error('Path to your private key require.');
 
-function parseLargeNumberRequest(data) {
+function parseLargeNumberSignatureRequest(data) {
     const params = {};
-
-    const paramRegex = /"(\w)": (\d+)/g;
     let match;
-    while((match = paramRegex.exec(data)) !== null) {
+
+    const numberRegex = /"(\w)": (\d+)/g;
+    while((match = numberRegex.exec(data)) !== null) {
         params[match[1]] = match[1].length === 1 ? bigInt(match[2]) : match[2];
+    }
+
+    const signatureRegex = /"signature": "([A-Za-z0-9+/=]+)"/;
+    if((match = signatureRegex.exec(data)) !== null) {
+        params.signature = match[1];
     }
 
     return params;
@@ -50,16 +58,29 @@ async function sign(data) {
     return openssl.stdout.trim();
 }
 
+async function verify(signature, data) {
+    const file = await tmp.file();
+    await fs.outputFile(file.path, signature);
+
+    try {
+        const openssl = await exec(`echo -n '${data}' | openssl dgst -sha256 -verify ${path.join(__dirname, 'bank-public-key.pem')} -signature ${file.path}`, { shell: '/bin/bash' });
+        return openssl.stdout.trim() === 'Verified OK';
+    } catch(error) {
+        return false;
+    }
+}
+
 async function fetchParameters() {
-    return parseLargeNumberRequest(await request.get('/login/dh/parameters'));
+    return parseLargeNumberSignatureRequest(await request.get('/login/dh/parameters'));
 }
 
 async function login(A) {
-    return parseLargeNumberRequest(
+    return parseLargeNumberSignatureRequest(
         await request({
             uri: '/login/dh',
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
+            jar: jar,
             body: `{"username": "${process.env.PAC_USERNAME}", "A": ${A.toString()}}`
         })
     );
@@ -70,6 +91,7 @@ function sendConfirmation(data) {
         uri: '/gateway',
         method: 'POST',
         headers: { 'Content-Type': 'application/octet-stream' },
+        jar: jar,
         encoding: null,
         resolveWithFullResponse: true,
         body: data
@@ -77,11 +99,17 @@ function sendConfirmation(data) {
 }
 
 async function openSession() {
+    jar = request.jar();
+
     const { g, p } = await fetchParameters();
     const x = bigInt.randBetween('340282366920938463463374607431768211456', '1e100');
 
     const A = g.modPow(x, p);
-    const { k, B } = await login(A);
+    const { k, B, signature } = await login(A);
+
+    if(!(await verify(Buffer.from(signature, 'base64'), `${A},${B},${k},${process.env.PAC_USERNAME}`))) {
+        return await openSession();
+    }
 
     const AB = B.modPow(x, p);
 
@@ -89,12 +117,10 @@ async function openSession() {
     hash.update(Buffer.from(AB.toString(16), 'hex'));
     const sessionKey = hash.digest('hex');
 
-    const signature = await sign(`${A},${B},${k},UGLIX`);
-
     const crypted = await encrypt(JSON.stringify({
         method: 'POST',
         url: '/bin/login/dh/confirmation',
-        args: { signature: signature }
+        args: { signature: await sign(`${A},${B},${k},UGLIX`) }
     }), sessionKey);
 
     const confirmation = await sendConfirmation(crypted);
